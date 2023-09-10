8c1bc320bfc4a4ad1cf5c0fcbb484a03bcec5d7d
Jack CP <jclarkeparker@gmail.com>
2023-09-11 11:11:42 +1200

    [CHG] Configure for Render deployment
    - Created a render.yaml blueprint file
    - Altered setup_database.sh to resemble what is used by Dynamite App

--- setup_database.sh
+++ setup_database.sh
@@ -1,3 +1,10 @@
+#! /usr/bin/env bash
+
+# exit on error
+
+set -o errexit
+
+bundle install
 dropdb keyboard_lesson_manager --if-exists
 createdb keyboard_lesson_manager
 psql -d keyboard_lesson_manager < seed.sql
