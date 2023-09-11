061a6633e0022d92247ac47f7f0560ced3952738
Jack CP <jclarkeparker@gmail.com>
2023-09-11 18:37:51 +1200

    [CHG](render-config) stream seed.sql into db
    The format provided in the previous commit seems to work!
    Now attempting to stream the seed file into the db too.

--- setup_database.sh
+++ setup_database.sh
@@ -1,3 +1,3 @@
 PGPASSWORD=$DATABASE_PASSWORD psql $DATABASE_URL -c 'DROP DATABASE IF EXISTS keyboard_lesson_manager';
 PGPASSWORD=$DATABASE_PASSWORD psql $DATABASE_URL -c 'CREATE DATABASE keyboard_lesson_manager';
-psql "postgres://$DB_USER:$DATABASE_PASSWORD@$DB_HOST:$DB_PORT/keyboard_lesson_manager"
+psql "postgres://$DB_USER:$DATABASE_PASSWORD@$DB_HOST:$DB_PORT/keyboard_lesson_manager" < seed.sql;
