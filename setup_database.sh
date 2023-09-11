6f6cc03ba2c470af0f5d12ee78cfb44c8e1f6dd7
Jack CP <jclarkeparker@gmail.com>
2023-09-11 14:50:53 +1200

    [CHG] (render-config) alternative setup_database
    Dropping and Creating the database using psql and SQL strings instead
    of client applications, because Render is complaining about connection
    to the server with dropdb failing.
    
    It may be because I'm already connecting to the database using this
    port. I may need to specify a different port to connect via.
    
    I might also need to change the DATABASE_URL environment variable, to
    be the external connection string instead of the internal one, though
    in theory the internal connection string should work.

--- setup_database.sh
+++ setup_database.sh
@@ -1,10 +1,3 @@
-#! /bin/bash
-
-# exit on error
-
-set -o errexit
-
-bundle install
-dropdb keyboard_lesson_manager --if-exists
-createdb keyboard_lesson_manager
-psql -d keyboard_lesson_manager < seed.sql
+psql $DATABASE_URL 'DROP DATABASE IF EXISTS keyboard_lesson_manager';
+psql $DATABASE_URL 'CREATE DATABASE keyboard_lesson_manager';
+psql $DATABASE_URL -d keyboard_lesson_manager < seed.sql;
