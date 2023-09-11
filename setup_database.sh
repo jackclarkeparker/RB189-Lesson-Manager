2ec9b2ff0b8659f3dc785e5a5cf0429be8048af2
Jack CP <jclarkeparker@gmail.com>
2023-09-11 15:13:35 +1200

    [CHG] (render-config) Add -c flag to psql commands

--- setup_database.sh
+++ setup_database.sh
@@ -1,3 +1,3 @@
-psql $DATABASE_URL 'DROP DATABASE IF EXISTS keyboard_lesson_manager';
-psql $DATABASE_URL 'CREATE DATABASE keyboard_lesson_manager';
+psql $DATABASE_URL -c 'DROP DATABASE IF EXISTS keyboard_lesson_manager';
+psql $DATABASE_URL -c 'CREATE DATABASE keyboard_lesson_manager';
 psql $DATABASE_URL -d keyboard_lesson_manager < seed.sql;
